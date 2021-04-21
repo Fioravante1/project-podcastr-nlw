207258eca889866dabd779cbef60d2bd9982d86d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-04-20 22:40:51 -0300

    Finalizando o dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-import { Header } from "../components/Header";
-
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <p>Esse projeto</p>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
