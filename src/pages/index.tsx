c04118cfb8c40e3effbe308adf196613b812def6
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-04-22 11:27:44 -0300

    Cria o route dos episodios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import { format, parseISO } from 'date-fns';
+import Link from 'next/link'
 import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/coverterDurationsToTimeString';
@@ -11,7 +12,6 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -44,7 +44,9 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
                 />
 
               <div className={styles.episodeDetails}>
-                <a href="">{episode.title}</a>
+                <Link href={`/episode/${episode.id}`}>
+                  <a>{episode.title}</a>
+                </Link>
                 <p>{episode.members}</p>
                 <span>{episode.puplishedAt}</span>
                 <span>{episode.durationAsString}</span>
@@ -64,18 +66,20 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
 
        <table cellSpacing={0}>
         <thead>
-          <th></th>
-          <th>Podcast</th>
-          <th>Integrantes</th>
-          <th>Data</th>
-          <th>Duaração</th>
-          <th></th>
+            <tr>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duaração</th>
+            <th></th>
+          </tr>
         </thead>
         <tbody>
           {allEpisodes.map(episode => {
             return (
               <tr key={episode.id}>
-                <td tyle={{ width: 72 }}>
+                <td style={{ width: 72 }}>
                   <Image
                      width={120}
                      height={120}
@@ -85,7 +89,9 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
                   />
                 </td>
                 <td>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episode/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                 </td>
                 <td>{episode.members}</td>
                 <td>{episode.puplishedAt}</td>
@@ -124,7 +130,6 @@ export  const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
     };
   })
